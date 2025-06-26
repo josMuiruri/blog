d55822e227b275d8d3fb439037cba416e658df8e
josMuiruri <josemuiruri51@gmail.com>
2025-06-26 17:46:01 +0300

    retrieve single post / all posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_4o4, render
+from django.http import Http404
+from . models import Post
+
 
 # Create your views here.
+def post_detail(request, id):
+    post = get_object_or_4o4(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.htlm', {'post': post})
+    #try:
+    #    post = Post.published.get(id=id)
+    #except Post.DoesNotExist:
+    #    raise Http404("No Post found.")
+    #return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_list(request):
+    posts = Post.publish.all()
+    return render(request, 'blog/post/list.html', {'post': posts})
