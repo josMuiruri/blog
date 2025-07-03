0e8094dbe48197ccafd02c6f908fb47bcbdbacab
josMuiruri <josemuiruri51@gmail.com>
2025-07-04 00:54:09 +0300

    changed post list from function-based to class based view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,15 +2,19 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from . models import Post
+from django.views.generic import ListView
 
 
 # Create your views here.
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
-    
+class PostListView(ListView):
+    """Alternative way"""
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'blog/post/list.html'
 
-def post_list(request):
+
+"""def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 5)
     page_number = request.GET.get('page', 1)
@@ -21,3 +25,9 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
+"""
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
