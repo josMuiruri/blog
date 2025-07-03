6ec7cd37771381f172309681b3fa335a1a898a47
josMuiruri <josemuiruri51@gmail.com>
2025-07-03 15:17:23 +0300

    implemented pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from . models import Post
@@ -10,5 +11,8 @@ def post_detail(request, year, month, day, post):
     
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
