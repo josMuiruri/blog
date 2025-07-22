a0406bf82c720f839acb11cc92958789f801b262
josMuiruri <josemuiruri51@gmail.com>
2025-07-22 05:30:26 +0300

    addition of post sharing

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,8 @@ from django.http import Http404
 from . models import Post
 from django.views.generic import ListView
 
+from . forms import EmailPostForm 
+
 
 # Create your views here.
 class PostListView(ListView):
@@ -31,3 +33,14 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404( Post, id=post_id, status=Post.Status.PUBLISHED )
+    if request.method == 'POST':
+        form = EmailPostForm(request.post)
+        if form.is_valid():
+            cd = form.cleaned_data
+        else:
+            form = EmailPostForm()
+    return render( request, 'blog/post/share.html', { 'post': post, 'form': form } )
