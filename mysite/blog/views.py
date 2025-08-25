6061f04f3b3ed7dc46e3805fef0436e5797c025d
josMuiruri <josemuiruri51@gmail.com>
2025-08-25 19:09:39 +0300

    implementation of emailing

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from . models import Post
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from . forms import EmailPostForm 
 
@@ -36,11 +37,43 @@ def post_detail(request, year, month, day, post):
 
 
 def post_share(request, post_id):
-    post = get_object_or_404( Post, id=post_id, status=Post.Status.PUBLISHED )
+    post = get_object_or_404( 
+        Post, 
+        id=post_id, 
+        status=Post.Status.PUBLISHED 
+    )
+    sent = False
+
     if request.method == 'POST':
         form = EmailPostForm(request.post)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_url( 
+                post.get_absolute_url()
+            )
+            subject = ( 
+                f"{cd['name']} ({cd['email']}) " 
+                f"recommends you read {post.title}" 
+            )
+            message = ( 
+                f"Read {post.title} at {post_url}\n\n" 
+                f"{cd['name']}\'s comments: {cd['comments']}" 
+            )
+            send_mail(
+                subject=subject, 
+                message=message, 
+                from_email=None, 
+                recipient_list=[cd['to']] 
+            )
+            sent = True
         else:
             form = EmailPostForm()
-    return render( request, 'blog/post/share.html', { 'post': post, 'form': form } )
+    return render( 
+        request, 
+        'blog/post/share.html', 
+        { 
+            'post': post, 
+            'form': form, 
+            'sent': sent 
+        }, 
+    )
